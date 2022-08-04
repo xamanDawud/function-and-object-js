601c04b8b252fcab64521f193a9027449ba90d26
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-04 20:52:16 +0600

    Ami tho purai agun..! function diye sum koria halachi

--- perameter.js
+++ perameter.js
@@ -4,3 +4,24 @@ function benson(money) {
 }
 
 benson(23);
+
+////////////////////////////////////////
+
+function taratariDenMiya(num1, num2) {
+    let lalChaPrice = num1;
+    let dudChaPrice = num2;
+    console.log(lalChaPrice + dudChaPrice);
+}
+
+let taka1 = 20;
+let taka2 = 50;
+taratariDenMiya(taka1, taka2);
+
+//////////////////////////////////////
+
+function add(a, b, c, d, e) {
+    let sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+add(23, 32, 3, 5, 66);
